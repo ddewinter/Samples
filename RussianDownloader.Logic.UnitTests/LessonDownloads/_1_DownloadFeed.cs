a8b4fccbf455f7cc2b9d2d2d6da1d9e726accc27
David DeWinter <david.dewinter@gmail.com>
2012-04-08 18:29:07 -0700

    Ensures IssueWebRequest returns input state.

--- RussianDownloader.Logic.UnitTests/LessonDownloads/_1_DownloadFeed.cs
+++ RussianDownloader.Logic.UnitTests/LessonDownloads/_1_DownloadFeed.cs
@@ -1,43 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
-
-namespace RussianDownloader.Logic.UnitTests.LessonDownloads
+﻿namespace RussianDownloader.Logic.UnitTests.LessonDownloads
 {
-    using System.IO;
-    using System.Threading.Tasks;
+    using System;
 
     using FluentAssertions;
+
+    using NUnit.Framework;
 
     [TestFixture]
     public class _1_DownloadFeed
     {
+        private readonly FeedDownloader _feedDownloader = new FeedDownloader();
+
         [Test]
         public void DownloadFeedSequence_should_issue_web_request_then_convert_to_XElement()
         {
             // Arrange
-            var subjectUnderTest = new FeedDownloader();
+            FeedDownloader subjectUnderTest = _feedDownloader;
 
             // Assert
             subjectUnderTest.DownloadFeedSequence.Should().Equal(
                 new Func<DownloadFeedState, DownloadFeedState>[]
-                {
-                    FeedDownloader.IssueWebRequest,
-                    FeedDownloader.ConvertStreamToXml
-                });
+                    { FeedDownloader.IssueWebRequest, FeedDownloader.ConvertStreamToXml });
+        }
+
+        [Test]
+        public void IssueWebRequest_should_return_input_state()
+        {
+            // Arrange
+            var state = new DownloadFeedState();
+
+            // Act
+            DownloadFeedState resultState = FeedDownloader.IssueWebRequest(state);
+
+            // Assert
+            resultState.Should().Be(state);
         }
 
         [Test]
         public void IssueWebRequest_should_populate_FeedResponse()
         {
             // Arrange
-            var subjectUnderTest = new FeedDownloader();
             var state = new DownloadFeedState();
 
             // Act
-            var resultState = FeedDownloader.IssueWebRequest(state);
+            FeedDownloader.IssueWebRequest(state);
 
             // Assert
             state.FeedResponse.Should().NotBeNull();
