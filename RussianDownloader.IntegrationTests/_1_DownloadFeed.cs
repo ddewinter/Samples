5371958b119454e266804bb7bcb265e678080721
David DeWinter <david.dewinter@gmail.com>
2012-04-13 16:13:37 -0700

    UrlResourceAccessor now successfully makes web requests.

---
+++ RussianDownloader.IntegrationTests/_1_DownloadFeed.cs
@@ -0,0 +1,30 @@
+﻿namespace RussianDownloader.IntegrationTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    using NUnit.Framework;
+
+    using RussianDownloader.Logic.Simulators;
+
+    using FluentAssertions;
+
+    [TestFixture]
+    public class _1_DownloadFeed
+    {
+        [Test]
+        public void UrlResourceAccessor_should_request_uri()
+        {
+            var subjectUnderTest = new UrlResourceAccessor();
+
+            var stream = subjectUnderTest.GetResourceStream("http://www.example.com");
+
+            var result = new StreamReader(stream.Result).ReadToEnd();
+
+            result.Should().Contain("iana");
+        }
+    }
+}
--- RussianDownloader.Logic/Simulators/UrlResourceAccessor.cs
+++ RussianDownloader.Logic/Simulators/UrlResourceAccessor.cs
@@ -1,13 +1,19 @@
 namespace RussianDownloader.Logic.Simulators
 {
     using System.IO;
+    using System.Net;
     using System.Threading.Tasks;
 
     public class UrlResourceAccessor : IResourceAccessor
     {
         public Task<Stream> GetResourceStream(string resourceLocation)
         {
-            throw new System.NotImplementedException();
+            var request = WebRequest.Create(resourceLocation);
+
+            return Task<Stream>.Factory.FromAsync(
+                request.BeginGetResponse,
+                (result) => request.EndGetResponse(result).GetResponseStream(),
+                null);
         }
     }
 }
