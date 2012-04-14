baa0d71caf873c2c27953e67f214017309be1ecb
David DeWinter <david.dewinter@gmail.com>
2012-04-14 16:22:54 -0700

    Add R# equality members to Location class.

--- RussianDownloader.Logic/Simulators/Location.cs
+++ RussianDownloader.Logic/Simulators/Location.cs
@@ -1,22 +1,82 @@
 namespace RussianDownloader.Logic.Simulators
 {
+    using System;
     using System.Net;
 
-    public class Location
+    public class Location : IEquatable<Location>
     {
+        public Location(string locationUri) :
+            this(locationUri, null)
+        {
+        }
+
         public Location(string locationUri, NetworkCredential credentials)
         {
             LocationUri = locationUri;
             Credentials = credentials;
         }
 
-        public Location(string locationUri) :
-            this(locationUri, null)
-        {
-        }
-
         public string LocationUri { get; set; }
 
         public NetworkCredential Credentials { get; set; }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(Location other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            return Equals(other.LocationUri, LocationUri) && Equals(other.Credentials, Credentials);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
+        /// </summary>
+        /// <returns>
+        /// true if the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>; otherwise, false.
+        /// </returns>
+        /// <param name="obj">The <see cref="T:System.Object"/> to compare with the current <see cref="T:System.Object"/>. </param><filterpriority>2</filterpriority>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != typeof(Location))
+            {
+                return false;
+            }
+            return Equals((Location)obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current <see cref="T:System.Object"/>.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((LocationUri != null ? LocationUri.GetHashCode() : 0) * 397) ^ (Credentials != null ? Credentials.GetHashCode() : 0);
+            }
+        }
     }
 }
