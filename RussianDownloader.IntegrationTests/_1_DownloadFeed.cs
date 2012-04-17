8d0f253c6b805d36c98ece091c330f8a2de5ae32
David DeWinter <david.dewinter@gmail.com>
2012-04-16 19:45:35 -0700

    Adds integration test for URL requests that require Basic authentication.

--- RussianDownloader.IntegrationTests/_1_DownloadFeed.cs
+++ RussianDownloader.IntegrationTests/_1_DownloadFeed.cs
@@ -8,6 +8,7 @@
 
     using NUnit.Framework;
 
+    using RussianDownloader.Logic;
     using RussianDownloader.Logic.Simulators;
 
     using FluentAssertions;
@@ -25,6 +26,25 @@
             var result = new StreamReader(stream.Result).ReadToEnd();
 
             result.Should().Contain("iana");
+        }
+
+        [Test]
+        public void UrlResourceAccessor_should_request_uri_with_credentials()
+        {
+            var subjectUnderTest = new UrlResourceAccessor();
+
+            var stream =
+                subjectUnderTest.GetResourceStream(
+                    new Location(
+                        "http://www.russianpod101.com/premium_feed/feed.xml",
+                        new Credentials("daviddewinter3985", "Moonraker003")));
+
+            using (var streamReader = new StreamReader(stream.Result))
+            {
+                var line = streamReader.ReadLine();
+
+                line.Should().Contain("<?xml");
+            }
         }
     }
 }
